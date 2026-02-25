a894138123e129a6a35032e7cc9eebd0c22f45c4
chigo <chigozie.dev@gmail.com>
2026-02-25 16:12:45 +0100

    feat(data): create network module for di

--- app/src/main/java/com/chigo/githubusersapp/di/NetworkModule.kt
+++ app/src/main/java/com/chigo/githubusersapp/di/NetworkModule.kt
@@ -1,1 +1,68 @@
 package com.chigo.githubusersapp.di
+
+import com.chigo.githubusersapp.BuildConfig
+import com.chigo.githubusersapp.data.remote.api.GitHubApiService
+import com.chigo.githubusersapp.data.util.API_TIMEOUT
+import com.chigo.githubusersapp.data.util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRequestInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val original = chain.request()
+            val newRequest = original.newBuilder()
+                .header("Accept", "application/vnd.github.v3+json")
+                .build()
+            chain.proceed(newRequest)
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(requestInterceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(API_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(API_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(API_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(requestInterceptor)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG) {
+                        HttpLoggingInterceptor.Level.BODY
+                    } else {
+                        HttpLoggingInterceptor.Level.NONE
+                    }
+                }
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideGitHubApiService(retrofit: Retrofit): GitHubApiService =
+        retrofit.create(GitHubApiService::class.java)
+}
