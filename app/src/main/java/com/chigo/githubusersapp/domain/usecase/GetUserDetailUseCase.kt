043934724d309ba63f66a871006a9e45e517171e
chigo <chigozie.dev@gmail.com>
2026-02-25 16:24:48 +0100

    feat(domain): create Usecases to get users and get users

--- app/src/main/java/com/chigo/githubusersapp/domain/usecase/GetUserDetailUseCase.kt
+++ app/src/main/java/com/chigo/githubusersapp/domain/usecase/GetUserDetailUseCase.kt
@@ -1,1 +1,16 @@
 package com.chigo.githubusersapp.domain.usecase
+
+import com.chigo.githubusersapp.domain.model.UserDetail
+import com.chigo.githubusersapp.domain.repository.UserDetailRepository
+import com.chigo.githubusersapp.data.util.BaseResponse
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class GetUserDetailUseCase @Inject constructor(
+    private val repository: UserDetailRepository
+) {
+    operator fun invoke(username: String): Flow<BaseResponse<UserDetail>> {
+        return repository.getUserDetail(username)
+    }
+}
--- app/src/main/java/com/chigo/githubusersapp/domain/usecase/GetUsersUseCase.kt
+++ app/src/main/java/com/chigo/githubusersapp/domain/usecase/GetUsersUseCase.kt
@@ -1,1 +1,16 @@
 package com.chigo.githubusersapp.domain.usecase
+
+
+import androidx.paging.PagingData
+import com.chigo.githubusersapp.domain.model.User
+import com.chigo.githubusersapp.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetUsersUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    operator fun invoke(): Flow<PagingData<User>> {
+        return repository.getUsers()
+    }
+}
