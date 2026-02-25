36898b2d14ce32e0d5e6c8ba6e28c506cb5134b8
chigo <chigozie.dev@gmail.com>
2026-02-25 16:26:52 +0100

    feat(ui): implement UserListViewModel

--- app/src/main/java/com/chigo/githubusersapp/presentation/features/userlist/UserListViewModel.kt
+++ app/src/main/java/com/chigo/githubusersapp/presentation/features/userlist/UserListViewModel.kt
@@ -1,1 +1,38 @@
 package com.chigo.githubusersapp.presentation.features.userlist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.chigo.githubusersapp.domain.model.User
+import com.chigo.githubusersapp.domain.usecase.GetUsersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserListViewModel @Inject constructor(
+    private val getUsersUseCase: GetUsersUseCase
+) : ViewModel() {
+
+    private val _users = MutableStateFlow<PagingData<User>>(PagingData.empty())
+    val users = _users.asStateFlow()
+
+    init {
+        getUsers()
+    }
+
+    private fun getUsers() {
+        viewModelScope.launch {
+            getUsersUseCase()
+                .distinctUntilChanged()
+                .cachedIn(viewModelScope)
+                .collect { pagingData ->
+                    _users.value = pagingData
+                }
+        }
+    }
+}
