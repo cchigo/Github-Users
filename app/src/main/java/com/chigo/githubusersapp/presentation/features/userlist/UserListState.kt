325a8d307fc4faa201d534c434174af5f9d500d6
chigo <chigozie.dev@gmail.com>
2026-02-25 17:22:22 +0100

    "feat(user_list_ui): create userlist state with all possible states in userlist screen"

--- app/src/main/java/com/chigo/githubusersapp/presentation/features/userlist/UserListState.kt
+++ app/src/main/java/com/chigo/githubusersapp/presentation/features/userlist/UserListState.kt
@@ -1,1 +1,15 @@
 package com.chigo.githubusersapp.presentation.features.userlist
+
+import androidx.paging.PagingData
+import com.chigo.githubusersapp.domain.model.User
+
+
+sealed class UserListState {
+    object Idle : UserListState()
+    object InitialLoading : UserListState()
+    object Refreshing : UserListState()
+    object Appending : UserListState()
+    data class Success(val users: PagingData<User>) : UserListState()
+    data class Error(val message: String) : UserListState()
+    data class AppendError(val message: String) : UserListState()
+}
